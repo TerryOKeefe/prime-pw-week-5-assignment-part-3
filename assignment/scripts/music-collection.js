b1bf58e937ca40400dbc5695d7af5e9ed0202b04
Terry O'Keefe <terry.okeefe87@gmail.com>
2021-04-26 18:53:40 -0500

    addToCollection function created

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -2,3 +2,19 @@ console.log('***** Music Collection *****')
 
 // create a variable called collection that starts as an empty array
 const collection = []; // my collection
+
+// add a function named addToCollection
+// input parameters of title, artist, yearPublished
+function addToCollection( title, artist, yearPublished, array){
+  const album = {
+    Title: title,
+    Artist: artist,
+    YearPublished: yearPublished,
+  }
+  array.push(album);
+  return album;
+}
+
+//
+console.log('Add album', addToCollection('Lies for the Liars', 'The Used', 2007, collection));
+console.log(collection);
