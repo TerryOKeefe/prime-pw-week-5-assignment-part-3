b1c52f1ebd009bde1774c5903a69cc6ef0cfd172
Terry O'Keefe <terry.okeefe87@gmail.com>
2021-04-26 19:10:42 -0500

    added 6 albums to collection

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -15,6 +15,25 @@ function addToCollection( title, artist, yearPublished, array){
   return album;
 }
 
-//
+// Add 6 Albums to test function
+
+// Album 1
 console.log('Add album', addToCollection('Lies for the Liars', 'The Used', 2007, collection));
+
+// Album 2
+console.log('Add album', addToCollection('Nothing Personal', 'All Time Low', 2009, collection));
+
+// Album 3
+console.log('Add album', addToCollection('Pioneer', 'The Maine', 2011, collection));
+
+// Album 4
+console.log('Add album', addToCollection('Disgusting', 'Beartooth', 2014, collection));
+
+// Album 5
+console.log('Add album', addToCollection('The Same Old Blood Rush With a New Touch', 'Cute Is What We Aim For', 2006, collection));
+
+// Album 6
+console.log('Add album', addToCollection('Epiphany', 'T-Pain', 2007, collection));
+
+// console log the collection array
 console.log(collection);
